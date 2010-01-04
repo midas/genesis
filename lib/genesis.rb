b22edf6a73653435dfcea3fa278ec2ad2a85130c
Jason Harrelson <jason@lookforwardenterprises.com>
2010-01-04 14:13:49 -0600

    Fixed includ efile issue that was causing a bug when using genesis with bundler gem.

--- lib/genesis.rb
+++ lib/genesis.rb
@@ -1,10 +1,11 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'genesis/active_record_extensions'
 require 'genesis/seeder'
 
 module Genesis
-  VERSION = '0.0.1'
+  VERSION = '0.0.3'
 end
 
 ActiveRecord::Base.send :include, Genesis::ActiveRecordExtensions if defined? ActiveRecord::Base
