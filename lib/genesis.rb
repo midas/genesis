a0283a431ce446ab87e1b32844ec9805914f46f2
Jason Harrelson <jason@lookforwardenterprises.com>
2013-03-24 11:07:10 -0500

    Rework loading of constants.

--- lib/genesis.rb
+++ lib/genesis.rb
@@ -1,13 +1,11 @@
-#$:.unshift(File.dirname(__FILE__)) unless
-  #$:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-require 'genesis/active_record_extensions'
-require 'genesis/railtie'
-require 'genesis/schema_seed'
-require 'genesis/seeder'
+require 'genesis/railtie' if defined?( Rails )
 
 module Genesis
-  SEEDS_ROOT = 'db/seeds'
-end
 
-#ActiveRecord::Base.send :include, Genesis::ActiveRecordExtensions if defined? ActiveRecord::Base
+  SEEDS_ROOT = 'db/seeds'
+
+  autoload :ActiveRecordExtensions, 'genesis/active_record_extensions'
+  autoload :SchemaSeed,             'genesis/schema_seed'
+  autoload :Seeder,                 'genesis/seeder'
+
+end
